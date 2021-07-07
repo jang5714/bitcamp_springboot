bfba15fd2e5f3aeeb3a954d781686c3d7c26613c
jang5714 <lionx3@naver.com>
2021-07-07 18:38:51 +0900

    july 0707, final

---
+++ src/main/java/com/example/demo/BankAccount/controller/BankAccountController.java
@@ -0,0 +1,23 @@
+package com.example.demo.BankAccount.controller;
+
+import com.example.demo.BankAccount.domain.BankAccount;
+
+import java.util.Scanner;
+
+public class BankAccountController {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        BankAccount bankAccount = new BankAccount();
+
+        System.out.println("계좌 번호는 :");
+        bankAccount.setAccNumber(scanner.next());
+        System.out.println("주민 번호는 :");
+        bankAccount.setSsNumber(scanner.next());
+        System.out.println("얼마를 넣으시겟습니가?");
+        bankAccount.setAmount(scanner.nextInt());
+
+        System.out.println(bankAccount.toString());
+
+
+    }
+}
---
+++ src/main/java/com/example/demo/BankAccount/domain/BankAccount.java
@@ -0,0 +1,45 @@
+package com.example.demo.BankAccount.domain;
+
+public class BankAccount {
+    private String accNumber; // 계좌 번호
+    private String ssNumber;  // 주민 번호
+    private int balance;   // 통장
+    private int amount; //입금
+
+    public void setAccNumber(String accNumber){
+        this.accNumber = accNumber;
+    }
+    public String getAccNumber(){
+        return this.accNumber;
+    }
+    public void setSsNumber(String ssNumber){
+        this.ssNumber = ssNumber;
+    }
+    public String getSsNumber(){
+        return this.ssNumber;
+    }
+    public void setBalance(int balance){
+        this.balance = balance;
+    }
+    public int getBalance(){
+        return this.balance;
+    }
+    public void setAmount(int amount){
+        this.amount = amount;
+    }
+    public int getAmount(){
+        return this.amount;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("계좌번호 : %s\n주민번호 : %s\n통장잔액 : %d"+accNumber + ssNumber +(balance-amount));
+    }
+    /*
+    @Override
+    public String toString() {
+        return String.format("계좌번호 : %s\n주민번호 : %s\n통장잔액 : %d"+accNumber + ssNumber +(balance-amount));
+    }
+
+     */
+}
