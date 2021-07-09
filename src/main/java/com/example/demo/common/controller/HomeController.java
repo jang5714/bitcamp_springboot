489e76b69105ba05d91e5d00532549ec8fb66a51
jang5714 <lionx3@naver.com>
2021-07-09 17:19:21 +0900

    july 0709

--- src/main/java/com/example/demo/common/controller/HomeController.java
+++ src/main/java/com/example/demo/common/controller/HomeController.java
@@ -1,16 +1,20 @@
 package com.example.demo.common.controller;
 
+import com.example.demo.math.controller.CalculatorController;
+
 import java.util.Scanner;
 
 public class HomeController {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        CalculatorController calculatorController = new CalculatorController();
         while (true){
             System.out.println("\n[메뉴] 0.종료 1.계산기 2.수열");
             switch (scanner.next()){
                 case "0" : return;
-                case "1" :
-                    System.out.println("실행");
+                case "1" : calculatorController.calculate();
+                    break;
+                case "2" : calculatorController.sequence();
                     break;
             }
         }
