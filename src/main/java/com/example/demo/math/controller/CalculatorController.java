6cffc21a056564c1073e1ad0b75340382206517c
jang5714 <lionx3@naver.com>
2021-07-09 12:23:06 +0900

    july 0709

--- src/main/java/com/example/demo/math/controller/CalculatorController.java
+++ src/main/java/com/example/demo/math/controller/CalculatorController.java
@@ -10,8 +10,23 @@ import java.util.Scanner;
 public class CalculatorController {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        CalculatorService calculatorService = new Calculatorlmpl();
         CalculatorDTO calculator = new CalculatorDTO();
+        CalculatorService calculatorService = new Calculatorlmpl();
+        System.out.println("수열의 시작값");
+        calculator.setNum1(scanner.nextInt());
+        System.out.println("수열의 마지막 값");
+        calculator.setNum2(scanner.nextInt());
+
+        int[] arr = calculatorService.sequence(calculator);
+        for(int i =0;i< arr.length; i++){ //arr의 길이 만큼 출력을 하겟다
+            System.out.print(arr[i]+"\t");
+        }
+        /*
+        int[] arr = calculatorService.sequence1(null);
+        for (int i=0;i< arr.length; i++){
+            System.out.print(arr[i]+"\t");
+        }
+        /*
         System.out.println("숫자 1 ?");
         calculator.setNum1(scanner.nextInt());
         System.out.println("연산자");
@@ -37,6 +52,6 @@ public class CalculatorController {
                 calculator.getOpcode(),
                 calculator.getNum2(),
                 result);
-
+    */
     }
 }
--- src/main/java/com/example/demo/math/sevice/Calculatorlmpl.java
+++ src/main/java/com/example/demo/math/sevice/Calculatorlmpl.java
@@ -27,4 +27,22 @@ public class Calculatorlmpl implements CalculatorService{
     public int remain(CalculatorDTO calculator) { //나머지
         return calculator.getNum1()%calculator.getNum2();
     }
+
+
+
+    @Override
+    public int[] sequence(CalculatorDTO calculator) {
+        int count = calculator.getNum2() - calculator.getNum1() + 1;//
+        int [] arr = new int[count]; //배열 크기 설정
+        int i = 0; //index 값(입력값)
+        for (int j =calculator.getNum1(); j<=calculator.getNum2(); j++){ //값
+            arr[i]=j;
+            i++;
+        }
+        return arr;
+    }
+
+
+
+
 }
